503b25c84cabbb687c5434309ed49619bd134a79
bibixx <Bartek@MacBook-Bartek.local>
2016-05-22 18:21:41 +0200

    Added charts

---
+++ chart.js
@@ -0,0 +1,253 @@
+$(function () {
+  var highchartsOptions = Highcharts.setOptions({
+    lang: {
+      months: ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'],
+      weekdays: ['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'],
+      shortMonths: ['sty', 'lut', 'mar', 'kwi', 'maj', 'cze', 'lip', 'sie', 'wrz', 'paź', 'lis', 'gru'],
+      contextButtonTitle: "Chart context menu",
+      decimalPoint: ",",
+      downloadPNG: "Pobierz w PNG",
+      downloadJPEG: "Pobierz w JPEG",
+      downloadPDF: "Pobierz w PDF",
+      downloadSVG: "Pobierz w SVG",
+      loading: 'Ładowanie...',
+      noData: "Brak danych do wyświetlenia",
+      printChart: "Wydrukuj wykres",
+      thousandsSep: " "
+    }
+  });
+
+  var buttons = {
+    buttons: {
+      contextButton: {
+        'stroke-width': 2,
+        symbolStroke: "#666",
+        symbolStrokeWidth: 1,
+        y: 0,
+        // width: 44,
+        // height: 24,
+        // symbolSize: 15,
+        // symbolX: 22,
+        // symbolY: 12,
+        theme: {
+          'stroke-width': 1,
+          stroke: '#ddd',
+          fill: '#fff',
+          r: 4,
+          states: {
+            hover: {
+              fill: '#ddd',
+              stroke: '#ddd',
+            },
+            select: {
+              fill: '#ddd',
+              stroke: '#ddd',
+            }
+          }
+        }
+      }
+    }
+  }
+
+  $('#container').highcharts({
+    chart: {
+      type: 'spline'
+    },
+    title: {
+      text: 'Wartości mierzonego ciśnienia krwi',
+      widthAdjust: -60
+    },
+    credits: {
+      enabled: false
+    },
+    xAxis: {
+        type: 'datetime',
+        dateTimeLabelFormats: {
+          month: '%e. %b',
+          year: '%b'
+        },
+        title: {
+          text: 'Data'
+        }
+    },
+    yAxis: {
+        title: {
+          text: 'Wartość ciśnienia (mmHg)'
+        },
+        gridLineColor: '#ccc',
+        gridLineWidth: 1,
+        alternateGridColor: null,
+        plotBands: [{
+            from: 122,
+            to: 147,
+            color: "rgba(255, 0, 0, 0.1)",
+            label: {
+                style: {
+                    color: '#606060'
+                }
+            }
+        }, {
+            from: 70,
+            to: 97,
+            color: "rgba(0, 120, 255, 0.1)",
+            label: {
+                style: {
+                    color: '#606060'
+                }
+            }
+        }]
+    },
+    tooltip: {
+        valueSuffix: ' mmHg'
+    },
+    exporting: buttons,
+    plotOptions: {
+        spline: {
+            lineWidth: 4,
+            states: {
+                hover: {
+                    lineWidth: 5
+                }
+            },
+            tooltip: {
+            	dateTimeLabelFormats: {
+              	millisecond:"%A, %e %b, %H:%M:%S.%L",
+                second:"%A, %e %b, %H:%M:%S",
+                minute:"%A, %e %b, %H:%M",
+                hour:"%A, %e %b, %H:%M",
+                day:"%A, %e %b, %Y",
+                week:"Week from %A, %e %b, %Y",
+                month:"%B %Y",
+                year:"%Y"
+              }
+            }
+        }
+    },
+    series: [{
+        name: 'SYS',
+        color: "rgba(255, 0, 0, 1)",
+        data: sysData
+    }, {
+        name: 'DIA',
+        color: "rgba(0, 110, 255, 1)",
+        data: diaData
+    }],
+    navigation: {
+        menuItemStyle: {
+            fontSize: '10px'
+        }
+    }
+  });
+
+  Highcharts.getOptions().plotOptions.pie.colors = (function () {
+      var colors = [],
+          base = Highcharts.getOptions().colors[8],
+          i;
+
+      for (i = 0; i < 10; i += 1) {
+          colors.push(Highcharts.Color(base).brighten((i - 3) / 7).get());
+      }
+      return colors;
+  }());
+
+  $('#container2').highcharts({
+      chart: {
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false,
+          type: 'pie'
+      },
+      title: {
+        text: 'Procentowy podział normy wartości ciśnienia skurczowego',
+        widthAdjust: -60
+      },
+      credits: {
+      	enabled: false
+      },
+      tooltip: {
+          enabled: true,
+          pointFormat: '<b>{point.y}</b>',
+      },
+      exporting: buttons,
+      plotOptions: {
+          pie: {
+              allowPointSelect: false,
+              dataLabels: {
+                  enabled: true,
+                  format: '{point.percentage:.1f}% ({point.y})',
+                  distance: -60
+              },
+              showInLegend: true
+          }
+      },
+      series: [{
+          colorByPoint: true,
+          data: [{
+              name: 'Poniżej normy',
+              y: (sysStats[0]>0) ? sysStats[0] : null
+          }, {
+              name: 'W normie',
+              y: sysStats[1]
+          }, {
+              name: 'Powyżej normy',
+              y: sysStats[2]
+          }]
+      }]
+  });
+
+  Highcharts.getOptions().plotOptions.pie.colors = (function () {
+      var colors = [],
+        base = Highcharts.getOptions().colors[0],
+        i;
+
+      for (i = 0; i < 10; i += 1) {
+        colors.push(Highcharts.Color(base).brighten((i - 3) / 7).get());
+      }
+      return colors;
+  }());
+
+  $('#container3').highcharts({
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+      },
+      title: {
+        text: 'Procentowy podział normy wartości ciśnienia rozkurczowego',
+        widthAdjust: -60
+      },
+      credits: {
+        enabled: false
+      },
+      tooltip: {
+          enabled: true,
+          pointFormat: '<b>{point.y}</b>',
+      },
+      exporting: buttons,
+      plotOptions: {
+          pie: {
+              allowPointSelect: false,
+              dataLabels: {
+                  enabled: true,
+                  format: '{point.percentage:.1f}% ({point.y})',
+                  distance: -60
+              },
+              showInLegend: true
+          }
+      },
+      series: [{
+          colorByPoint: true,
+          data: [{
+              name: 'Poniżej normy',
+              y: (diaStats[0]>0) ? diaStats[0] : null
+          }, {
+              name: 'W normie',
+              y: diaStats[1]
+          }, {
+              name: 'Powyżej normy',
+              y: diaStats[2]
+          }]
+      }]
+  });
+});
--- graph.php
+++ graph.php
@@ -18,11 +18,19 @@ if( ($auth == true) ){
   <body>
     <?php include 'nav.php'; ?>
 
-    <div class="container">
+    <div class="container-fluid">
 
       <div class="body-main">
 
-        <img src="graph_img.php" alt="" />
+        <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+        <div class="row-fluid">
+          <div class="col-xs-12 col-md-6">
+            <div id="container2" style="margin: 0 auto"></div>
+          </div>
+          <div class="col-xs-12 col-md-6">
+            <div id="container3" style="margin: 0 auto"></div>
+          </div>
+        </div>
 
       </div>
 
@@ -31,6 +39,76 @@ if( ($auth == true) ){
 
     <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script type="text/javascript">
+<?php
+  $sysLimit = array(122, 147);
+  $diaLimit = array(70, 97);
+  $sysStats = array(0,0,0);
+  $diaStats = array(0,0,0);
+  $sys = "";
+  $dia = "";
+
+  $sort =  "`date` ASC";
+  $arrLocales = array('pl_PL', 'pl','Polish_Poland.28592');
+  setlocale( LC_ALL, $arrLocales );
+  $dbc = mysql_connect('localhost', 'root', 'admin') or die( 'błąd' );
+  $dcs = mysql_select_db('pressure');
+
+  $query = "SELECT * FROM `pressures` ORDER BY".$sort;
+  $data = mysql_query($query);
+
+  while ($row = mysql_fetch_array($data)) {
+    $dtime = DateTime::createFromFormat("Y-m-d H:i:s", $row["date"]);
+    $t = $dtime->getTimestamp();
+
+    $sysVal = $row["sys"];
+    $diaVal = $row["dia"];
+
+    $sys .= "  [Date.UTC(".date("Y, ", $t).(date("n", $t)-1).date(", j, G, i", $t)."), $sysVal],\n";
+    if( $sysVal>$sysLimit[1] ){
+      $sysStats[2] += 1;
+    } else if( $sysVal<$sysLimit[0] ){
+      $sysStats[0] += 1;
+    } else {
+      $sysStats[1] += 1;
+    }
+
+    $dia .= "  [Date.UTC(".date("Y, ", $t).(date("n", $t)-1).date(", j, G, i", $t)."), $diaVal],\n";
+    if( $diaVal>$diaLimit[1] ){
+      $diaStats[2] += 1;
+    } else if( $diaVal<$diaLimit[0] ){
+      $diaStats[0] += 1;
+    } else {
+      $diaStats[1] += 1;
+    }
+
+  }
+
+  $sys = rtrim($sys, "\n");
+  $sys = rtrim($sys, ",");
+  $sys .= "\n";
+
+  $dia = rtrim($dia, "\n");
+  $dia = rtrim($dia, ",");
+  $dia .= "\n";
+
+  echo "var sysData = [\n".$sys;
+  echo "];\n\n";
+
+  echo "var diaData = [\n".$dia;
+  echo "];\n\n";
+
+  echo "var sysLimit = ".json_encode($sysLimit).";\n";
+  echo "var diaLimit = ".json_encode($diaLimit).";\n\n";
+  echo "var sysStats = ".json_encode($sysStats).";\n";
+  echo "var diaStats = ".json_encode($diaStats).";\n";
+
+  mysql_close($dbc);
+?>
+    </script>
+    <script src="chart.js"></script>
   </body>
 </html>
 
